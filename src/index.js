cad11fb0642aca8d0f842fbac5049ea47fc7de9b
aravanirmala <142471184+aravanirmala@users.noreply.github.com>
2024-06-05 13:29:35 +0530

    imagegpt

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useRef, useState } from 'react';
+const Bot = () => {
+  const [name, setName] = useState('flowers')
+  const [imgg, setImgg] = useState('https://loremflickr.com/1600/800/dog')
+  const url = "https://loremflickr.com/1600/800/"
+  const data = useRef()
+
+  function image() {
+    console.log(url + data.current.value)
+    setName(data.current.value)
+    setImgg(url + data.current.value)
+  }
+
+  return (
+    <div className='bot-container'>
+      <div className='left-container'>
+        <div className='left-container-content'>
+          <h1>JHC BOT</h1>
+          <button className='left-btn'>+ New Chat</button>
+        </div>
+
+        <div>
+          <button className='simple-btn'>cars</button>
+        </div>
+        <div>
+          <button className='simple-btn'>Food</button>
+        </div>
+        <div>
+          <button className='simple-btn'>Flowers</button>
+        </div>
+        <div>
+          <button className='simple-btn'>Bikes</button>
+        </div>
+        <div>
+          <button className='simple-btn'>Buildings</button>
+        </div>
+
+      </div>
+
+      <div className='right-container'>
+        <div className='right-container-content'>
+          <div className="alert alert-warning alert-dismissible fade show bot-alert" role="alert">
+            <strong>Don't Search!</strong> any abusing content
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+          <div className='solution'>
+            <h1>{name}</h1>
+            <img src={imgg}></img>
+          </div>
+          <div className='chat-container'>
+            <input type="text" placeholder='ex:pig ,donkey.....' ref={data}></input>
+            <button onClick={image}>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(< Bot />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
